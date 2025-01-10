cd3c0a13cd9824aa2efeaa377c2c834cbde5b6fa
SoraTheFox <alexanderjackstephenson@gmail.com>
2025-01-10 13:24:40 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@
 // ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
 // │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
 // └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+var api = process.env.WeatherAPI
 
 const CONFIG = {
 	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
@@ -26,7 +27,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: process.env.WeatherAPI, // Write here your API Key
+	weatherKey: api, // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
